dd713487c549bb220cd4f294b37d1d2bbd600f45
Pankhuri <pankhuritrikha@gmail.com>
2020-07-25 21:38:25 +0530

    Input is handled

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Search from './components/Search';
 
 function App() {
+
+  const [state, setState]= useState ({
+    s : "", //pull in the search query in the s
+    results : [],
+    selected : {} //movie we clicked
+  });
+
+  const apiurl="http://www.omdbapi.com/?i=tt3896198&apikey=cda02f3b";
+
+  const handleInput = (e) =>  {
+    let s=e.target.value;
+
+    setState(prevState => {
+        return { ...prevState,s: s}
+    });
+
+    console.log(state.s);
+  }
+
   return (
     <div className="App">
       <header>
@@ -9,7 +28,7 @@ function App() {
       </header>
       <main>
         {/*  Search component for searching the movies */}
-        <Search />
+        <Search handleInput={handleInput} />
         {/* Results component for the results of the movies shown */}
         {/* Popup component for the details of every movie */}
       </main>
--- moviedb/src/components/Search.js
+++ moviedb/src/components/Search.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Search = () => {
+const Search = ({handleInput}) => {
     return (
         <section className="searchbox-wrap">
-           <input type="text" placeholder="Search for a movie..." className="searchbox"/>
+           <input type="text" placeholder="Search for a movie..." className="searchbox" onChange={handleInput}/>
         </section>
     )
 }
