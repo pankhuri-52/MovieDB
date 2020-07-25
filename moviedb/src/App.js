d68353e284fd9842d9353c02633ecdeb0c5eddd3
Pankhuri <pankhuritrikha@gmail.com>
2020-07-25 21:14:43 +0530

    Search-Bar ready with styling

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Search from './components/Search';
 
 function App() {
   return (
@@ -6,6 +7,12 @@ function App() {
       <header>
        <h1>Movie Database</h1>
       </header>
+      <main>
+        {/*  Search component for searching the movies */}
+        <Search />
+        {/* Results component for the results of the movies shown */}
+        {/* Popup component for the details of every movie */}
+      </main>
     </div>
   );
 }
---
+++ moviedb/src/components/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <section className="searchbox-wrap">
+           <input type="text" placeholder="Search for a movie..." className="searchbox"/>
+        </section>
+    )
+}
+export default Search;
